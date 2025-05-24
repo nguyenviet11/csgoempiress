d14fd48589154d14407a3bacbc583c683b1bb7dc
vietnv <nguyenvanvietbn1998@gmail.com>
2025-05-25 02:01:26 +0700

    update

--- src/app/csgoempires-connecting/page.js
+++ src/app/csgoempires-connecting/page.js
@@ -6,12 +6,12 @@ import { FcOk } from "react-icons/fc";
 import { useEffect } from "react";
 
 export default function page() {
-    useEffect(() => {
-      const timeout = setTimeout(() => {
-        window.location.href = "https://blogreview.pro/skins/review-csgoempire";
-      }, 300);
-      return () => clearTimeout(timeout);
-      }, []);
+    // useEffect(() => {
+    //   const timeout = setTimeout(() => {
+    //     window.location.href = "https://blogreview.pro/skins/review-csgoempire";
+    //   }, 300);
+    //   return () => clearTimeout(timeout);
+    //   }, []);
   return (
     <div>
       <main className={styles.main}>
